ca61b4693fe93317e87bc321e3510bb354a368d8
AlbertoVanMeenen <61149327+AlbertoVanMeenen@users.noreply.github.com>
2020-04-29 11:40:02 +0200

    Update temp.py
    inputs part 2

--- DSE/ConceptEvaluation/temp.py
+++ DSE/ConceptEvaluation/temp.py
@@ -1,13 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Used in the conceptual stage to estimate main parameters for concept selection
+Outputs are
+Payload range diagram
+Weight
+Volume
+Cost 
 """
+
 from numpy import*
 
-def RangeJet(V, cj, L_D, Weight_frac):
-	""" This function computes the range that can be obtained by a 
+
+
+# ------------------------ INPUTS ------------------------
+
+# ---------------------- Constants ----------------------
+g = 9.81
+M = 0.78
+
+
+# ---------------------- Variables ----------------------
+h = 10970 #km = FL360
+
+cj = 1.132*10**(-5)
+L_D = 13
+Weight_frac = 1/0.8544
+
+# ------------------------ RANGE ------------------------
+
+
+
+def ISA_trop(h):
+    """ This function computes
+	Input:
+		
+	Output:
+		
+	"""
+    T = 288.15 -0.0065*h
+    p = 101325*(T/288.15)**(-g/(-0.0065*287))
+    rho = 1.225 * (T/288.15)**(-g/(-0.0065*287)-1)
+    a = sqrt(1.4*287*T)
+    
+    return T,p,rho,a
+    
+
+
+
+def RangeJet(g, M, a, cj, L_D, Weight_frac):
+    
+    """ This function computes the range that can be obtained by a 
 		jet aircraft
 	Input:
 		V = cruise velocity [m/s]
@@ -17,6 +59,17 @@ def RangeJet(V, cj, L_D, Weight_frac):
 	Output:
 		R = Range [m]
 	"""
-	g = 9.80665;
-	R = (V/(cj*g))*L_D*ln(Weight_frac);
-	return R;
+    
+    V = M * a
+    R = (V/(cj*g))*L_D*log(Weight_frac)
+    
+    return R
+
+
+
+# ------------------------ MAIN -----------------------
+
+T, p, rho, a = ISA_trop(h)
+R = RangeJet(g, M, a, cj, L_D, Weight_frac)
+
+print(R)
