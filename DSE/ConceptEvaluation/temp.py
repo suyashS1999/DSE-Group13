0a226ace34a9f22644766443e5efaab5b78236a7
suyashS1999 <suyash1999@gmail.com>
2020-04-29 11:06:12 +0200

    Range equation

--- DSE/ConceptEvaluation/temp.py
+++ DSE/ConceptEvaluation/temp.py
@@ -4,4 +4,19 @@ Spyder Editor
 
 This is a temporary script file.
 """
+from numpy import*
 
+def RangeJet(V, cj, L_D, Weight_frac):
+	""" This function computes the range that can be obtained by a 
+		jet aircraft
+	Input:
+		V = cruise velocity [m/s]
+		cj = Thrust specific fuel consumption [kg/Ns]
+		L_D = Lift to Drag ratio
+		Weight_frac = weight fraction before and after cruise phase [-]
+	Output:
+		R = Range [m]
+	"""
+	g = 9.80665;
+	R = (V/(cj*g))*L_D*ln(Weight_frac);
+	return R;
