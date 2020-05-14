e5f1a8172e0911e063490015e4f8964cb4ee20bd
suyashS1999 <suyash1999@gmail.com>
2020-05-14 13:34:27 +0200

    Seperated ascent and descent phase

--- DSE/ConceptEvaluation/Concept2.py
+++ DSE/ConceptEvaluation/Concept2.py
@@ -29,12 +29,13 @@ A = array([9, 10, 11]);							# Aspect ratio [-]
 W_S_max = 7000;									# Max Wing Loading value, change this value if you want to change the range of wing loading values you want to assess [N/m^2]
 n = 2.5;										# Maximum load factor [-]
 reserve_fuel_frac = 0.1;						# Reserve fuel fraction [-]
-ascent_descent_fuel_frac = 0.09;				# Ascent and Descent fuel fraction [-]
+pre_cruise_fuel_frac = 0.15;					# Pre cruise fuel fraction [-]
+post_cruise_fuel_frac = 0.18;					# Post cruise fuel fraction [-]
 #%% ------------------------ Main ------------------------
 T, p, rho_cruise, a = ISA_trop(h);
 sigma_cruise = rho_cruise/rho0;			# Density ratio [-]
 v_cruise = M*a;							# Cruise speed
-PayloadRangeDiagram_JET(MTOW, OEW, Payload_max, reserve_fuel_frac, ascent_descent_fuel_frac, Max_Fuel_cap, (g, M, a, cj, L_D));
+PayloadRangeDiagram_JET(MTOW, OEW, Payload_max, reserve_fuel_frac, pre_cruise_fuel_frac, post_cruise_fuel_frac, Max_Fuel_cap, (g, M, a, cj, L_D));
 
 fig = plt.figure(figsize = (10, 8));
 _ = W_S_stall(v_stall_landing, Cl_max, MLW/MTOW, fig);
