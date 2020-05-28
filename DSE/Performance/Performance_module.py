3df2ac16d4a73d3fca2774082d9b92e67cecb15b
kei998 <keikei1215@aol.com>
2020-05-28 18:32:38 +0200

    braced+BLI concept payload range and things

--- DSE/Performance/Performance_module.py
+++ DSE/Performance/Performance_module.py
@@ -1,6 +1,7 @@
 from PayloadRangeDiagram import *
 from WingandThrustLoadingDiagrams import *
 
+#%% ---------------------- Constants ----------------------
 g = 9.81;										# Gravitaional acceleration [m/s^2]
 rho0 = 1.225;									# Sea level air Density [kg/m^3]
 M = 0.78;										# Cruise mach number [-]
@@ -9,7 +10,7 @@ MTOW = 76446.83536*g;							# Max Take off Weight [N]
 MLW = 0.86*MTOW									# Max Landing Weight [N]
 OEW = 42939.92655*g;							    # Operational empty weight [N]
 Payload_max = 20*1000*g;						# Payload weight [N]
-Max_Fuel_cap = 18*1000*g;						# Max fuel weight [N]
+Max_Fuel_cap = 17.9*1000*g;						# Max fuel weight [N]
 cj = 0.0000143;							        # Specific fuel consumption [kg/Ns]
 L_D = 20.41115173;								# Lift to drag ratio [-]
 h = 11000;										# Cruise altitude [m]
@@ -22,8 +23,8 @@ rho_airport = 1.225;							# air density at runway altitude [kg/m^3]
 sigma = rho_airport/rho0;						# Density ratio [-]
 N_engines = 2;									# Number of engines [-]
 #c_v = 0.01199;	(now specified inside function)	# Climb gradient divied by velocity [-]
-Cl_max = array([2.5,2.7,2.9]);							# Cl max values for assessment [-]
-Cl_max_takeoff = array([2.1,2.3,2.5]);				 	# Cl_max for take off [-]
+Cl_max = array([2.4, 2.5]);							# Cl max values for assessment [-]
+Cl_max_takeoff = array([2.0, 2.1]);				 	# Cl_max for take off [-]
 A = array([17]);								# Aspect ratio [-]
 W_S_max = 7000;									# Max Wing Loading value, change this value if you want to change the range of wing loading values you want to assess [N/m^2]
 n = 2.5;										# Maximum load factor [-]
