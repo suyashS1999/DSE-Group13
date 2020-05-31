59a6f74b086c057a85afd46e14ac88f0d9f73599
kei998 <keikei1215@aol.com>
2020-05-31 18:34:22 +0200

    Updated flight envelope code, runs with DUMMY values

--- DSE/Performance/Performance_module1.py
+++ DSE/Performance/Performance_module1.py
@@ -7,42 +7,81 @@
 #%% ---------------------- Imports ----------------------
 import numpy as np
 import matplotlib.pyplot as plt
+from ConceptEvaluation.PayloadRangeDiagram import ISA_trop
 
 #%% ---------------------- Constants ----------------------
 g = 9.81                        # Gravitational acceleration [m/s^2]
 M_cruise = 0.78                 # Mach number in cruise [-]
 h_cruise = 11000                # Altitude in cruise [m]
-rho0 = 1.225                    # Sea level air Density [kg/m^3]
 
 #%% ---------------------- Inputs ----------------------
-MTOW = 74616.9829*g             # MTOW [N]
-S_ref =  142.520611             # Reference wing area [m^2]
-CL_max_clean = 1                # DUMMY Max CL with clean configuration
-n1 = 2.5                        # Positive limit maneuvering load factor [-]
+MTOW = 74616.9829                   # MTOW [kg]
+S_ref =  142.520611                 # Reference wing area [m^2]
+CL_max_clean = 1.5                  # DUMMY Max CL with clean configuration [-]
+CL_min_clean = -0.8*CL_max_clean    # DUMMY negative Max CL with clean configuration [-]
+CL_max_flap = 2.0                   # MAX CL with LANDING CONFIGURATION (need to be determined) [-]
+n_max = 2.5                         # Positive limit maneuvering load factor [-]
+n_min = -1                          # Negative limit maneuvering load factor [-]
+n_flap = 2                          # Assumed (from 2nd year project guide)
 #%% ---------------------- Functions ----------------------
-#Copy pasted for now, cannot import .py files inside other folders
-def ISA_trop(h):
-	""" This function computes the atmospheric properties 
-		within the troposphere
-	Input:
-		h = altitude [m]
-	Output:
-		T = Temperature [K]
-		p = Pressure [Pa]
-		rho = Densituy [kg/m^3]
-		a = spped of sound [m/s]
-	"""
-	T = 288.15 - 0.0065*h;
-	p = 101325*(T/288.15)**(-g/(-0.0065*287));
-	rho = 1.225*(T/288.15)**(-g/(-0.0065*287) - 1);
-	a = sqrt(1.4*287*T);
-	return T, p, rho, a;
+
 
 
 #%% ---------------------- Main ----------------------
-T, p, rho, a = ISA_trop(h)
-W_cruise = MTOW;                                            # For now I assume W is MTOW during cruise
-V_stall = np.sqrt(W_cruise/S_ref*2/rho/CL_max_clean)        # Stall speed [m/s]
-V_maneuver = V_stall*np.sqrt(n1)                            # Maneuver speed [m/s]
-V_cruise = a*M_cruise                                       # Cruise speed as function of M [m/s]
-V_dive = V_cruise/0.8                                      # Dive speed
+T, p, rho, a = ISA_trop(h_cruise)
+T0, p0, rho0, a0 = ISA_trop(0)
+
+W_cruise = MTOW*g;                                          # For now I assume W is MTOW during cruise
+V_stall_clean = np.sqrt(W_cruise/S_ref*2/rho0/CL_max_clean) # Stall speed EAS [m/s]
+V_stall_flap = np.sqrt(W_cruise/S_ref*2/rho0/CL_max_flap)   # DUMMY Stall speed EAS with flaps extended
+V_maneuver = V_stall_clean*np.sqrt(n_max)                   # Maneuver speed EAS [m/s]
+V_cruise = a0*M_cruise*np.sqrt(p/p0)                        # Cruise speed EAS [m/s]
+V_dive = V_cruise/0.8                                       # Dive speed EAS [m/s]
+V_neg_stall_clean = np.sqrt(W_cruise/S_ref*2/rho0/-CL_min_clean)
+V_max_flap = 0.85*V_maneuver                                 # Max speed with flaps extended
+
+#Maneuver envelope
+fig = plt.figure(figsize = (12, 8))
+plt.xlabel("Equivalent airspeed, $V_{EAS}$")
+plt.ylabel("Load factor, n")
+plt.axhline(y = 0, color = "black")
+plt.xlim(0, V_dive + 10)
+
+#Limiting velocities EAS
+plt.vlines(x = V_dive, ymin = 0, ymax = n_max)
+plt.text(V_dive, 0, '$V_D$', ha='left', va='bottom')
+#V_cruise and V_dive linear 
+plt.plot([V_cruise, V_dive], [n_min, 0], color = "black")
+#Origin to n_max clean
+V_positive_clean = np.linspace(0, V_maneuver, 100)
+n_positive_clean = (V_positive_clean/V_stall_clean)**2
+plt.plot(V_positive_clean, n_positive_clean, color = "black")
+#Origin to n_min
+n_negative_clean = np.linspace(0, n_min, 100)
+V_negative_clean = np.sqrt(-n_negative_clean)*V_neg_stall_clean
+plt.plot(V_negative_clean, n_negative_clean, color = "black")
+#Limiting loads
+plt.hlines(y = n_max, xmin = V_maneuver, xmax = V_dive)
+plt.hlines(y = n_min, xmin = V_neg_stall_clean, xmax = V_cruise)
+
+#Flaps extended
+#Origin to n_max flaps
+n_positive_flap = np.linspace(0, n_flap, 100)
+V_positive_flap = np.sqrt(n_positive_flap/2)*V_stall_flap
+plt.plot(V_positive_flap, n_positive_flap, "--", color = "black")
+#Horizontal line
+plt.hlines(y = n_flap, xmin = V_positive_flap[-1], xmax = V_max_flap)
+#Vertical line
+plt.vlines(x = V_max_flap, ymin = 0, ymax = n_flap, linestyles = "--")
+plt.text(V_max_flap, 0, '$V_F$', ha='left', va='bottom')
+
+#Dotted lines to help make it easier to see
+plt.vlines(x = V_cruise, ymin = n_min, ymax = n_max, linestyles = "--")
+plt.text(V_cruise, 0, '$V_C$', ha='left', va='bottom')
+plt.hlines(y = 1, xmin = 0, xmax = V_dive, linestyles = "--")
+plt.vlines(x = V_stall_clean, ymin = 0, ymax = 1, linestyles = "--")
+plt.text(V_stall_clean, 0, '$V_{S1}$', ha='left', va='bottom')
+plt.vlines(x = V_stall_flap, ymin = 0, ymax = 2, linestyles = "--")
+plt.text(V_stall_flap, 0, '$V_{S0}$', ha='left', va='bottom')
+plt.vlines(x = V_maneuver, ymin = 0, ymax = (V_maneuver/V_stall_clean)**2, linestyles = "--")
+plt.text(V_maneuver, 0, '$V_A$', ha='left', va='bottom')
