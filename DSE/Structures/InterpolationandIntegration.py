c846db1099b8ab363e59becb9520bf5df29040d6
suyashS1999 <suyash1999@gmail.com>
2020-06-07 02:32:02 +0200

    Added Cm interpolation

--- DSE/Structures/InterpolationandIntegration.py
+++ DSE/Structures/InterpolationandIntegration.py
@@ -40,7 +40,7 @@ def RBF(r):
 	Output:
 		phi = RBF evaluated at r
 	"""
-	epi = 260;
+	epi = 10;
 	phi = sqrt(1 + (epi*r)**2);
 	return phi;
 
@@ -64,23 +64,40 @@ def RBF_1DInterpol(nodes, f, plot_nodes):
 
 
 
+
 #%% --------------- Main -----------------------
-dir_CL = r"C:\Users\Gebruiker\source\repos\DSE\DSE\Structures\liftdistribution.txt";
-dir_Cm = r"C:\Users\Gebruiker\source\repos\DSE\DSE\Structures\troquedistribution.txt";
+dir_CL = "liftdistribution.txt";
+dir_Cm = "troquedistribution.txt";
 data_CL = genfromtxt(dir_CL);
 data_Cm = genfromtxt(dir_Cm);
 CL = data_CL[1, :];												# CL values
 Cm = data_Cm[1, :];												# Cm values around centre of gravity
+centre_pressure = data_Cm[2, :];								# x location of centre of pressure measured from LE root chord
 span_location = data_CL[0, :];									# Spanwise stations
-Lift_dist = CL*0.5*rho_cruise*V_cruise**2*ave_chord;			# Distributed lift load in N/m
+Lift_dist = CL*0.5*rho_cruise*V_cruise**2;						# Distributed lift load in N/m
+Moment_dist = Cm*0.5*rho_cruise*V_cruise**2*ave_chord;			# Distributed pitching moment load in Nm/m
 
-x = linspace(span_location[0], span_location[-1], 100);
-CL_lag = Lagrange_Basis(span_location, Lift_dist, x);
-CL_rbf = RBF_1DInterpol(span_location, Lift_dist, x);
+x = linspace(span_location[0], span_location[-1], 100);			# plot_nodes
+CL_lag = Lagrange_Basis(span_location, Lift_dist, x);			# Interpolate Distributed lift with Lagrange basis functions
+CL_rbf = RBF_1DInterpol(span_location, Lift_dist, x);			# Interpolate Distributed lift with radial basis functions
+Cm_lag = Lagrange_Basis(span_location, Moment_dist, x);			# Interpolate Distributed pitching moment with Lagrange basis functions
+Cm_rbf = RBF_1DInterpol(span_location, Moment_dist, x);			# Interpolate Distributed pitching moment with radial basis functions
+
 plt.figure(figsize = (18, 8));
 plt.plot(span_location, Lift_dist, "x");
 plt.plot(x, CL_lag, label = "Lagrange Interpolation");
 plt.plot(x, CL_rbf, label = "RBF Interpolation");
+plt.ylabel("Distributed Lift Load [N/m]");
+plt.xlabel("Span [m]");
+plt.grid(True);
+plt.legend();
+
+plt.figure(figsize = (18, 8));
+plt.plot(span_location, Moment_dist, "x");
+plt.plot(x, Cm_lag, label = "Lagrange Interpolation");
+plt.plot(x, Cm_rbf, label = "RBF Interpolation");
+plt.ylabel("Distributed Pitching Moment Load [N/m]");
+plt.xlabel("Span [m]");
 plt.grid(True);
 plt.legend();
 plt.show();
