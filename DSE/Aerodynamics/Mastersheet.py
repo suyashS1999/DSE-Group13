54bbfad6f2d0afe0351b147649adfefe89c03ff5
mikswart <mikswart@ziggo.nl>
2020-06-10 23:39:09 +0200

    Performed first iteration

---
+++ DSE/Aerodynamics/Mastersheet.py
@@ -0,0 +1,32 @@
+import numpy as np
+import Aero_tools as tl
+from Input_parm import *
+from Wing_estimation import *
+
+print("Start Mastersheet from 0")
+print("C_r_m = ", C_r_m, "\n")
+print("C_t_m = ", C_t_m, "\n")
+print("Ct_inb = ", Ct_inb, "\n")
+print("S = ", S, "\n")
+print("S_inb = ", S_inb, "\n")
+print("S_outb = ", S_outb, "\n")
+print("AR = ", AR, "\n")
+print("AR_inb = ", AR_inb, "\n")
+print("AR_outb = ", AR_outb, "\n")
+print("taper_ratio = ", taper_ratio, "\n")
+print("taper_ratio_inb = ", taper_ratio_inb, "\n")
+print("taper_ratio_outb = ", taper_ratio_outb, "\n")
+print("MAC = ", MAC, "\n")
+print("yMAC = ", yMAC, "\n")
+print("span = ", span, "\n")
+print("span_inboard = ", span_inboard, "\n")
+print("span_outboard = ", span_outboard, "\n")
+print("sweep_LE = ", sweep_LE, "\n")
+print("sweep_half_c = ", sweep_half_c, "\n")
+print("sweep_quart = ", sweep_quart, "\n")
+print("CL_alp = ", CL_alp, "\n")
+print("Total_CD_full = ", Total_CD_full, "\n")
+print("CL_alp_M02 = ", CL_alp_M02, "\n")
+print("CLmax_M02 = ", CLmax_M02, "\n")
+print("alpha_stall = ", alpha_stall, "\n")
+
