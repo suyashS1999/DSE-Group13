f4c35d555439236f83b9988ef28c233e73fcafb9
sanjayv99 <sanjayvermani.banga@gmail.com>
2020-06-14 18:16:02 +0200

    changes in aero elatic analysis

--- DSE/Aerodynamics/Aeroelas_analysis.py
+++ DSE/Aerodynamics/Aeroelas_analysis.py
@@ -6,57 +6,56 @@
 import numpy as np
 import Aero_elas_tools as tool
 from matplotlib import pyplot as plt
-# import Input_parm as inp
-# from Aero_tools import *
+import Input_parm as inp
+from Aero_tools import *
 
 
 
 
-c = 2
-# c = ave_chord      # Mean chord  
-# e = 0.25*ave_chord # distance between ac line and elastic line
-# half_span = span/2  # half span
-half_span = 7.5
-# sweep_half = sweep_x(0.5,sweep_LE,C_r_m,C_t_m,span) # radians
-y_a = 10.8
-x_f = 0.48*c
-e = (x_f/c) - (1/4)
+
+c = inp.ave_chord      # Mean chord  
+e = 0.25 # distance between ac line and elastic line
+half_span = inp.span/2  # half span
+half_area = inp.S*0.5
+sweep_half = sweep_x(0.5,inp.sweep_LE,inp.C_r_m,inp.C_t_m,inp.span) # radians
+y_a = 16
+x_f = 0.5*c
 m = 200# mass/area
 M_theta_dot = -1.2
 
-K_th = 1# Torsional stiffness
-K_phi = 1# Bending stiffness
 
-# CL_alp = CL_alpha_DATCOM(AR,M_cruise,a0_full,sweep_half) #wing life slope
-GJ = 2e6
+
+CL_alp = CL_alpha_DATCOM(inp.AR,inp.M_cruise,inp.a0_full,sweep_half) #wing life slope
+GJ = 2e7
 EI = 2e7
 
 
-E_f = 0.4  #control_chord/total_chord
-# rho = rho_cruise
-rho = 1.225
-# cl_a = a0_full
-cl_a = 2*np.pi
-# cm_d = tool.cl_d(cl_a,E_f)
-# cl_d = tool.cm_d(cl_a,E_f)
+E_f = 0.3  #control_chord/total_chord
+rho = inp.rho_cruise
+cl_a = inp.a0_full
+cm_d = tool.cm_d(cl_a,E_f)
+cl_d = tool.cl_d(cl_a,E_f)
 
 
 
-# v_div_str = tool.diverg_speed_straight(CL_alp,G,J,rho,e,half_span)
+v_div_str = (tool.diverg_speed_straight(CL_alp,GJ,rho,e,c,half_span))
 
-# v_div_swept = tool.diverg_q(K_th,K_phi,S,e,CL_alp,sweep_half,half_span)
+q_d = tool.diverg_q_v2(GJ,EI,half_span,e,CL_alp,c,sweep_half)
 
-# crit_sweep = tool.crit_sweep(e,c,half_span,K_th,K_phi)
+crit_sweep = tool.crit_sweep(EI,GJ,e,c,half_span)
 
-V_inf = np.linspace(10,170,1000)
+verifi = v_div_str - tool.diverg_q_v2(GJ,EI,half_span,e,CL_alp,c,0)
 
-# q_d = 0.5*rho*(v_div_swept**2)
-# ail_eff_full_s = tool.aileron_eff_full(rho,V_inf,CL_alp,q_d,K_th,cm_d,cl_d,c,S,sweep_half)
+V_inf = np.linspace(1,370,1000)
 
-# lamb = tool.lamb(rho,V_inf,e,cl_a,G,J)
+
+# ail_eff_full_s = tool.aileron_eff_full(rho,V_inf,CL_alp,q_d,K_th,cm_d,cl_d,c,half_area,sweep_half)
+
+lamb = tool.lamb(rho,V_inf,e,c,cl_a,GJ)
 
 # ail_eff_str_wing = tool.aileron_eff_act(lamb,half_span,y_a,e,cl_a,cl_d,c,cm_d)
 
+ail_eff_v2 = tool.aileron_eff_new(lamb,half_span,c,cm_d,cl_d,e,cl_a)
 
 dummy_1 = np.zeros((2,2))
 I_dummy = np.eye(2)
@@ -96,18 +95,18 @@ for i in range(len(V_inf)):
 
 print("Results \n \n")
 
-# print("Diverg Speed (Swept wing model)", v_div_swept,"\n")
-# print("Diverg Speed (Straight Wing", v_div_str,"\n")
-# print("Critical Sweep", crit_sweep,"\n")
+print("Diverg Speed (q_d) (Swept wing model)", q_d,"\n")
+print("Diverg Speed (q_d) (Straight Wing)", v_div_str,"\n")
+print("Critical Sweep", crit_sweep,"\n")
+print("Verification =",verifi)
 
 
-
-# plt.figure()
-# plt.plot(V_inf,ail_eff_full_s,label="full span aileron")
-# plt.plot(V_inf,ail_eff_str_wing,label="Actual aileron (no sweep and 2D coeff)")
-# plt.xlabel("V_inf")
-# plt.ylabel("Äileron effectiveness")
-# plt.grid()
+plt.figure()
+plt.plot(V_inf,ail_eff_v2,label="Actual aileron (no sweep and 2D coeff)")
+plt.xlabel("V_inf [m/s]")
+plt.ylabel("Äileron effectiveness")
+plt.legend()
+plt.grid()
 
 plt.figure()
 plt.subplot(2,1,1)
@@ -121,16 +120,3 @@ plt.xlabel("V_inf")
 plt.ylabel("Frequency")
 plt.title("Negative")
 
-# plt.figure()
-# plt.subplot(2,1,1)
-# plt.plot(V_inf,damp_2[:,0])
-# plt.plot(V_inf,damp_2[:,1])
-# plt.xlabel("V_inf")
-# plt.ylabel("Damping Ratio [%]")
-
-# plt.subplot(2,1,2)
-# plt.plot(V_inf,freq_2[:,0])
-# plt.plot(V_inf,freq_2[:,1])
-# plt.xlabel("V_inf")
-# plt.ylabel("Frequency")
-# plt.title("Postitive")
