787411e8810d274536c19ddd29a5b61b34f797a7
AlbertoVanMeenen <61149327+AlbertoVanMeenen@users.noreply.github.com>
2020-06-16 11:48:17 +0200

    original parameters

--- DSE/Structures/main_wingbox_design.py
+++ DSE/Structures/main_wingbox_design.py
@@ -43,8 +43,8 @@ n_stations = 10
 #design parameters
 #/!\ make sure each array contains n_station values
 
-t_top = np.array([0.0005,0.001,0.002,0.003,0.004,0.003,0.0005,0.005,0.003,0.002])
-t_bot = np.array([0.001,0.005,0.005,0.005,0.005,0.005,0.0015,0.003,0.001,0.001])
+t_top = np.array([0.0005,0.0005,0.001,0.0015,0.002,0.0015,0.0005,0.0025,0.0015,0.001])
+t_bot = np.array([0.001,0.0025,0.0025,0.0025,0.0025,0.0025,0.0015,0.0015,0.0005,0.0005])
 t_spar = np.array([0.002,0.003,0.004,0.005,0.005,0.004,0.003,0.002,0.001,0.001])
 
 n_stif_top = np.array([4,4,4,4,4,4,4,21,17,11])
@@ -55,7 +55,7 @@ print("Design Inputs are:")
 print("stiffener configuration top",n_stif_top)
 print("stiffener configuration bot",n_stif_bot)
 
-A_stif = 0.003*0.05*0.04
+A_stif = 0.002*0.05*0.04
 A_spar_cap = 0.005*0.07*0.05
 
 #load case
