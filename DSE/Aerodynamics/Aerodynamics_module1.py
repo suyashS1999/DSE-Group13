4d292d52b1c08fbff5951131f2ad9011c817f188
suyashS1999 <suyash1999@gmail.com>
2020-06-18 11:37:03 +0200

    Small changes to plots

--- DSE/Aerodynamics/Aerodynamics_module1.py
+++ DSE/Aerodynamics/Aerodynamics_module1.py
@@ -123,22 +123,22 @@ class ExtractData_OpenVSP():
 						if c == 0: CL_w = span; Cm_w = span; c += 1;
 						CL_w = vstack((CL_w, CL));
 						Cm_w = vstack((Cm_w, Cm, Centre_pressure));
-					ax1 = plt.subplot(2, 1, 1);
-					ax1.title.set_text("Lift Distribution {}".format(name[len(self.file_types[1][1:]) :]));
-					ax1.plot(span, CL, "x-", label = "alpha = {}".format(alpha));
-					ax1.set_xlabel("span [m]");
-					ax1.set_ylabel("Cl [-]");
-					ax1.grid(True);
+						ax1 = plt.subplot(2, 1, 1);
+						ax1.title.set_text("Lift Distribution {}".format(name[len(self.file_types[1][1:]) :]));
+						ax1.plot(span, CL, "x-", label = "alpha = {}".format(alpha));
+						ax1.set_xlabel("span [m]");
+						ax1.set_ylabel("Cl [-]");
+						ax1.grid(True);
 
-					ax2 = plt.subplot(2, 1, 2);
-					ax2.title.set_text("Drag Distribution {}".format(name[len(self.file_types[1][1:]) :]));
-					ax2.plot(span, CD, "^-", label = "alpha = {}".format(alpha));
-					ax2.set_ylabel("Cd [-]");
-					ax2.set_xlabel("span [m]");
-					ax2.grid(True);
+						ax2 = plt.subplot(2, 1, 2);
+						ax2.title.set_text("Drag Distribution {}".format(name[len(self.file_types[1][1:]) :]));
+						ax2.plot(span, CD, "^-", label = "alpha = {}".format(alpha));
+						ax2.set_ylabel("Cd [-]");
+						ax2.set_xlabel("span [m]");
+						ax2.grid(True);
 
-					ax1.legend(loc = "upper left");
-					ax2.legend(loc = "upper left");
+						ax1.legend(loc = "upper left");
+						ax2.legend(loc = "upper left");
 		if write == True:
 			savetxt(write_dir, CL_w);
 			savetxt(write_dir1, Cm_w);
@@ -193,7 +193,7 @@ write_dir = r"C:\Users\Gebruiker\source\repos\DSE\DSE\Structures\liftdistributio
 write_dir1 = r"C:\Users\Gebruiker\source\repos\DSE\DSE\Structures\troquedistribution.txt"	# Write directory
 #%% ------------------- Main -------------------
 vsp_data = ExtractData_OpenVSP(dir);
-AOA = 14;
+AOA = 7;
 CD0 = 0.01111;			# 0.01204
 vsp_data.plot_Polars(CD0);
 vsp_data.plot_LoadDistribution(AOA, CD0, write_dir, write_dir1);
